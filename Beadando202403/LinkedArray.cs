5c5364f092e55123550237e4617cb41a959800cf
Gyarmati Levente <gyarmatilevi@gmail.com>
2024-03-11 13:17:23 +0100

    start

--- Beadando202403/LinkedArray.cs
+++ Beadando202403/LinkedArray.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace Beadando202403
 {
-    public class LinkedArray
+    public class LinkedArray<T>
     {
+        private T[][] data;
+        public int count { get; private set; } = 0; // mennyi tombot tarolunk
+        public int size { get; private set; } // mekkorak a tarolt tombok
+
+        public LinkedArray(int size)
+        {
+            count++;
+            data = new T[count][];
+            this.size = size;
+        }
+
+        public T[] GetElement(int index)
+        {
+            if(index < count)
+            {
+                return data[index];
+            }
+            else
+            {
+                Console.WriteLine("Out of bounds");
+                return null;
+            }
+        }
+
+        public void SetElement(int index, T[] value)
+        {
+            if(index > count)
+            {
+                Console.WriteLine("Too big index");
+                return;
+            }
+            if(value.Length > size)
+            {
+                Console.WriteLine("Too big array");
+                return;
+            }
+
+            data[index] = value;           
+        }
+
+        public void Clear()
+        {
+            count = 1;
+            data = new T[count][];
+        }
+
+        public void SetZero()
+        {
+            size = 0;
+        }
+
+        public void NewRow()
+        {
+            count++;
+
+        }
     }
 }
--- Beadando202403/Program.cs
+++ Beadando202403/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            LinkedArray<int> test = new LinkedArray<int>(3);
 
+            int[] tomb = { 1, 2, 3, 4 ,5};
+            test.SetElement(0, tomb);
+
+            Console.WriteLine(test.GetElement(0));
         }
     }
 }
